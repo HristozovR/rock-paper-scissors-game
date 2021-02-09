784af88f2965390f20f8074786ec60fa4ace6d61
Roman Hristozov <romkata92@gmail.com>
2021-02-09 20:46:19 +0200

    Update JS and Animation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
+    let pScore = 0;
+    let cScore = 0;
 
     // Start the game function
     const startGame = () => {
@@ -19,6 +19,13 @@ const game = () => {
         const options = document.querySelectorAll('.options button');
         const playerHand = document.querySelector('.player-hand');
         const computerHand = document.querySelector('.computer-hand');
+        const hands = document.querySelectorAll('.hands img');
+
+        hands.forEach(hand => {
+            hand.addEventListener('animationend', function(){
+                this.style.animation = '';
+            });
+        });
         // Generate Computer Options
         const computerOptions = ['rock', 'paper', 'scissors'];
 
@@ -27,11 +34,87 @@ const game = () => {
                 // Computer choice
                 const computerNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[computerNumber];
-                console.log(computerChoice);
+                
+                setTimeout(() => {
+                    // Here we call compare hands
+                compareHands(this.textContent, computerChoice);
+
+                // Update Images
+                playerHand.src = `./images/${this.textContent}.png`;
+                computerHand.src = `./images/${computerChoice}.png`;
+                }, 2000);
+
+                // Animation
+                playerHand.style.animation = 'shakePlayer ease 2s';
+                computerHand.style.animation = 'shakeComputer ease 2s';
             });
         });
-        
-        
+    };
+
+    // Update Score 
+
+    const updateScore = () => {
+        const playerScore = document.querySelector('.player-score p');
+        const computerScore = document.querySelector('.computer-score p');
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    }
+
+    const compareHands = (playerChoice, computerChoice) => {
+        // Update text
+
+        const winner = document.querySelector('.winner');
+        // Checking for a tie
+
+        if (playerChoice === computerChoice) {
+            winner.textContent = 'It is a tie';
+            return;
+        }
+        // Check for rock
+
+        if (playerChoice === 'rock') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }else{
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+        // Check for paper
+
+        if (playerChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            }else{
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+        // Check for scissors
+
+        if (playerChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            }else{
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
     };
 
     // Calling all the inner function
